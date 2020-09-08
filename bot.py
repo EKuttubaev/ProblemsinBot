e36286a66de5a2bed2ccb4e77dc5613ca9cec2ff
Erkin Kuttubaev <kuttubaev@gmail.com>
2020-09-08 15:39:05 +0600

    Added buttons and created new branch

--- bot.py
+++ bot.py
@@ -1,12 +1,19 @@
 import config
 from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters
+from telegram import ReplyKeyboardMarkup,ReplyKeyboardRemove
 
 print("Бот запущен!")
 
 
 def on_start(update, context):
+    main_buttons = [
+        ["Продажа", "Категории"],
+        ["Отчеты", "История"]
+    ]
+    buttons = ReplyKeyboardMarkup(main_buttons)
     chat = update.effective_chat
-    context.bot.send_message(chat_id=chat.id, text="Привет! Я бот помощник.\n Выберите команду.")
+    context.bot.send_message(chat_id=chat.id, text="Привет! Я бот помощник.\n Выберите команду.",
+                             reply_markup=buttons)
 
 
 updater = Updater(config.token, use_context=True)
